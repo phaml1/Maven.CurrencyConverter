ffebead64294cec9019c544a783a7327d1cd40de
Loc <loc@Zipcoders-MacBook-Pro-3.local>
2023-12-02 00:31:12 -0500

    got CurrencyType

--- src/main/java/io/zipcoder/currencyconverterapplication/CurrencyType.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/CurrencyType.java
@@ -25,6 +25,32 @@ public enum CurrencyType {
     }
 
     public static CurrencyType getTypeOfCurrency(ConvertableCurrency currency) {
+        switch(currency.getClass().getSimpleName()){
+            case "AustralianDollar":
+                return CurrencyType.AUSTRALIAN_DOLLAR;
+            case "CanadianDollar":
+                return CurrencyType.CANADIAN_DOLLAR;
+            case "ChineseYR":
+                return CurrencyType.CHINESE_YR;
+            case "Euro":
+                return CurrencyType.EURO;
+            case "Franc":
+                return CurrencyType.FRANC;
+            case "Pound":
+                return CurrencyType.POUND;
+            case "Ringgit":
+                return CurrencyType.RINGGIT;
+            case "Rupee":
+                return CurrencyType.RUPEE;
+            case "SingaporeDollar":
+                return CurrencyType.SINGAPORE_DOLLAR;
+            case "UniversalCurrency":
+                return CurrencyType.UNIVERSAL_CURRENCY;
+            case "USDollar":
+                return CurrencyType.US_DOLLAR;
+            case "Yen":
+                return CurrencyType.YEN;
+        }
         return null;
     }
 }
